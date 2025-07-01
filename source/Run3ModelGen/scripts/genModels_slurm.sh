49853f05bc7efa0ccfbcdd1d29ee71ee794a28c5
Valentin Reichenspurner <v.reichenspurner@tum.de>
2025-07-01 10:43:58 +0200

    initial batch setup for slurm

--- source/Run3ModelGen/scripts/genModels_slurm.sh
+++ source/Run3ModelGen/scripts/genModels_slurm.sh
@@ -5,13 +5,13 @@
 # is valid for any single-threaded program, including
 # sequential Matlab, Mathematica, Julia, and similar cases.
 #
-#SBATCH -J modelgen-test
-#SBATCH --array=1-100
-#SBATCH -o ./slurm_run/out_%A/out.%a
-#SBATCH -e ./slurm_run/err_%A/err.%a
+#SBATCH -J modelgen
+#SBATCH --array=1-50
+#SBATCH -o ./out_%A/out.%a
+#SBATCH -e ./err_%A/err.%a
 #SBATCH -D ./
-#SBATCH --mem=8G
-#SBATCH --time=0:10:00
+#SBATCH --mem=2G
+#SBATCH --time=1-00
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=v.reichenspurner@tum.de
 
@@ -20,8 +20,8 @@ module load gcc/10 impi/2021.2
 module load anaconda/3/2021.05
 
 # Set number of OMP threads to fit the number of available cpus, if applicable.
-export OMP_NUM_THREADS=1
+# export OMP_NUM_THREADS=1
 
 # Run single-core program
 source $HOME/Run3ModelGen/build/setup.sh
-srun pixi run genModels.py --config_file $HOME/Run3ModelGen/config_files/MSSM19atQ.yaml --scan_dir $HOME/Run3ModelGen/runs/slurm_run/$SLURM_ARRAY_JOB_ID$SLURM_ARRAY_TASK_ID --custom_model True 
+srun pixi run genModels.py --config_file $HOME/Run3ModelGen/config_files/MSSM19atQ.yaml --seed $SLURM_ARRAY_JOB_ID$SLURM_ARRAY_TASK_ID --scan_dir /ptmp/vreich/modelgen/$SLURM_ARRAY_JOB_ID/$SLURM_ARRAY_TASK_ID --custom_model True 
